ce8edc4e579a9222993ccd080f11f072700c381f
TheBokiya <nomad.kaka@live.com>
2014-02-02 21:29:19 -0800

    Added a slider for year selection

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToolBar;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.CircleBuilder;
@@ -16,12 +21,37 @@ public class Main extends Application {
 	private String[] movies;
 	private int SCREEN_WIDTH = 1200;
 	private int SCREEN_HEIGHT = 700;
+	private int SELECTED_YEAR;
 
 	@Override
 	public void start(Stage stage) throws Exception {
 		// TODO Auto-generated method stub
 
 		Group root = new Group();
+		ToolBar toolbar = new ToolBar();
+		toolbar.setMinWidth(SCREEN_WIDTH);
+		toolbar.prefHeight(50);
+		
+		final Label yearLabel = new Label();
+		yearLabel.setText("Year: ");
+		
+		Slider yearSlider = new Slider();
+		yearSlider.setMin(1950);
+		yearSlider.setMax(2002);
+		yearSlider.setShowTickLabels(false);
+		yearSlider.setBlockIncrement(1);
+		yearSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> arg0,
+					Number arg1, Number arg2) {
+				SELECTED_YEAR = (int) Math.floor((Double) arg0.getValue());
+				yearLabel.setText("Year: " + SELECTED_YEAR);
+				System.out.println(SELECTED_YEAR);
+			}
+		});
+	
+		toolbar.getItems().addAll(yearSlider, yearLabel);
 
 		String csvFile = "movies.csv";
 		BufferedReader br = null;
@@ -44,10 +74,11 @@ public class Main extends Application {
 
 				Circle mov = CircleBuilder.create()
 						.centerX(Math.random() * SCREEN_WIDTH)
-						.centerY(Math.random() * SCREEN_HEIGHT).radius(2)
+						.centerY(Math.random() * SCREEN_HEIGHT+25).radius(2)
 						.fill(Color.web("D94D3F")).opacity(0.5).build();
 
 				root.getChildren().add(mov);
+				
 
 			}
 
@@ -65,9 +96,11 @@ public class Main extends Application {
 			}
 		}
 		System.out.println("Done");
+		
+		root.getChildren().add(toolbar);
 
 		stage.setTitle("Movies Visual Analytics");
-		stage.setScene(new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT));
+		stage.setScene(new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT+25));
 		stage.show();
 	}
 
